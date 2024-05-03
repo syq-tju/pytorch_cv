a568cd934997f8595d0701afb07a2064b00cc6df
Yuqi Shi <shiyuqi@zju.edu.cn>
2024-05-03 09:16:24 +0800

    torch.tensor fundamental

--- 02/torch_crash.py
+++ 02/torch_crash.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 print(torch.__version__)
 
 #print(torch.cuda.is_available())
@@ -15,3 +16,26 @@ print("x_1=",x_1)
 print("y  =",y)
 print("z_1=",z_1)
 print("z_2=",z_2)
+
+from math import pi
+x_general = torch.tensor([True, 1, pi], dtype=torch.float64)
+print(x_general)
+print(x_general.dtype)
+
+a = torch.ones((3,4))
+print("a = ",a)
+b = torch.zeros((3,4))
+print("b = ",b)
+c = torch.rand((3,4)) 
+print("c = ",c)
+d = torch.randn((3,4))
+print("d = ",d)
+e = torch.randint(0,10,(3,4))
+print("e = ",e)
+print(torch.empty((3,4)))
+
+#torch.tensor和numpy.array的相互转换
+x_numpy = np.array([[1,2,3],[4,5,6]])
+y_torch = torch.tensor(x_numpy)
+x_numpy_2 = np.array(y_torch)
+print(type(x_numpy),type(y_torch),type(x_numpy_2))
