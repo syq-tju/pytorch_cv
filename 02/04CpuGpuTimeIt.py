5921fee124166f610e7660fffda50109234b0f23
Yuqi Shi <shiyuqi@zju.edu.cn>
2024-05-04 08:51:50 +0800

    Compare time difference of a Gpu and a Cpu with Matrix Multiplication

--- 02/04CpuGpuTimeIt.py
+++ 02/04CpuGpuTimeIt.py
@@ -1,12 +1,29 @@
 import torch
+import timeit
 
-x = torch.tensor([1.0, 3.0, 5.0], requires_grad=True)
-y = torch.tensor([4.0, 5.0, 6.0], requires_grad=True)
+# 准备数据
+x = torch.rand(1, 64000)
+y = torch.rand(64000, 10240)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+x_cuda = x.to(device)
+y_cuda = y.to(device)
+x_cpu = x
+y_cpu = y
 
-#  out 包含 x 和 y 的计算
-out = (x.pow(2) + y.pow(2) + x*y).sum()
+# 定义要计时的函数
+def matmul_cuda():
+    torch.mm(x_cuda, y_cuda)
 
-out.backward()
+def matmul_cpu():
+    torch.mm(x_cpu, y_cpu)
 
-print(x.grad)  # 输出: tensor([ 2.,  6., 10.])
-print(y.grad)  # 输出: tensor([ 8., 10., 12.])
+# 使用 timeit 模块进行计时
+cuda_time = timeit.timeit("matmul_cuda()", globals=globals(), number=10)
+cpu_time = timeit.timeit("matmul_cpu()", globals=globals(), number=10)
+
+print(f"CUDA 计算时间: {cuda_time / 10} 秒")
+print(f"CPU 计算时间: {cpu_time / 10} 秒")
+
+#982 µs ± 55.4 ns per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+#20.2 ms ± 260 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
